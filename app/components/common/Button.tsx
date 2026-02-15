21d42db7a72b4982e6d832f5dac00c7687d86516
kais-1997 <47506kais@juniv.edu>
2026-02-16 02:21:33 +0600

    Button.tsx

--- app/components/common/Button.tsx
+++ app/components/common/Button.tsx
@@ -1,38 +1,60 @@
-"use client"
+"use client";
 
-// props
+import { IconType } from "react-icons";
+
 interface ButtonProps {
-  label: string;
-  submit?: boolean;
-  onClick?: () => void;
-  disabled?: boolean;
-  secondary?: boolean;
+	label: string;
+	onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+	disabled?: boolean;
+	outline?: boolean;
+	small?: boolean;
+	icon?: IconType;
+	secondary?: boolean; // This was the "Facebook Blue/Green" trigger
 }
 
-// Button
-// defaults to button type, can be submit type
-// primary button is blue, secondary button is green
 const Button: React.FC<ButtonProps> = ({
-  label,
-  submit = false,
-  onClick,
-  disabled,
-  secondary = false
+	label,
+	onClick,
+	disabled,
+	outline,
+	small,
+	icon: Icon,
+	secondary,
 }) => {
-  return (
-    <button
-      type={submit ? 'submit' : 'button'}
-      onClick={onClick}
-      disabled={disabled}
-      className={
-        `text-white font-bold py-2 px-4 rounded text-lg transition w-full
-        ${secondary
-          ? 'bg-[#35a420] hover:bg-[#30911d] max-w-fit mx-auto px-8'
-          : 'bg-[#1877f2] hover:bg-[#166ad9]'}`}
-    >
-      {label}
-    </button>
-  );
+	return (
+		<button
+			onClick={onClick}
+			disabled={disabled}
+			className={`
+        relative
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+        rounded-lg
+        hover:opacity-80
+        transition
+        w-full
+        ${outline ? "bg-white" : secondary ? "bg-[#ff8c00]" : "bg-[#ff8c00]"}
+        ${outline ? "border-black" : secondary ? "border-[#ff8c00]" : "border-[#ff8c00]"}
+        ${outline ? "text-black" : "text-white"}
+        ${small ? "py-1" : "py-3"}
+        ${small ? "text-sm" : "text-md"}
+        ${small ? "font-light" : "font-semibold"}
+        ${small ? "border-[1px]" : "border-2"}
+      `}
+		>
+			{Icon && (
+				<Icon
+					size={24}
+					className="
+            absolute
+            left-4
+            top-3
+          "
+				/>
+			)}
+			{label}
+		</button>
+	);
 };
 
 export default Button;
